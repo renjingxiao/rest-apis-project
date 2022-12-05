ffdf0e4da68d82f8321bf92dde5deb5517ae5342
unknown <you@example.com>
2022-12-05 13:38:06 +0000

    Add model changes and migrations, make sure migrations run before app atarts.

--- app.py
+++ app.py
@@ -97,8 +97,7 @@ def create_app(db_url=None):
             401,
         )
 
-    with app.app_context():
-        db.create_all()
+    
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
